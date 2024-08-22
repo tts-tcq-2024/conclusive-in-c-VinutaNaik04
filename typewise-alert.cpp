6c4c9934ba1a4ada54a8b9b3615a72cdb96e7f44
VinutaNaik04 <vinutanaik456@gmail.com>
2024-08-22 18:30:34 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,71 +1,74 @@
 #include "typewise-alert.h"
 #include <stdio.h>
 
+// Function to infer breach type based on temperature and limits
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
-  if(value < lowerLimit) {
-    return TOO_LOW;
-  }
-  if(value > upperLimit) {
-    return TOO_HIGH;
-  }
-  return NORMAL;
+    if (value < lowerLimit) {
+        return TOO_LOW;
+    }
+    if (value > upperLimit) {
+        return TOO_HIGH;
+    }
+    return NORMAL;
 }
 
-BreachType classifyTemperatureBreach(
-    CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = 0;
-  int upperLimit = 0;
-  switch(coolingType) {
-    case PASSIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 35;
-      break;
-    case HI_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 45;
-      break;
-    case MED_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 40;
-      break;
-  }
-  return inferBreach(temperatureInC, lowerLimit, upperLimit);
+// Function to classify temperature breach based on cooling type and temperature
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
+    int lowerLimit = 0;
+    int upperLimit = 0;
+
+    switch (coolingType) {
+        case PASSIVE_COOLING:
+            upperLimit = 35;
+            break;
+        case HI_ACTIVE_COOLING:
+            upperLimit = 45;
+            break;
+        case MED_ACTIVE_COOLING:
+            upperLimit = 40;
+            break;
+        default:
+            return NORMAL; // Default case for unknown cooling types
+    }
+
+    return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
-void checkAndAlert(
-    AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
-
-  BreachType breachType = classifyTemperatureBreach(
-    batteryChar.coolingType, temperatureInC
-  );
-
-  switch(alertTarget) {
-    case TO_CONTROLLER:
-      sendToController(breachType);
-      break;
-    case TO_EMAIL:
-      sendToEmail(breachType);
-      break;
-  }
+// Function to send alert based on breach type
+void sendAlert(AlertTarget alertTarget, BreachType breachType) {
+    switch (alertTarget) {
+        case TO_CONTROLLER:
+            {
+                const unsigned short header = 0xfeed;
+                printf("Header: %x, BreachType: %d\n", header, breachType);
+            }
+            break;
+        case TO_EMAIL:
+            {
+                const char* recipient = "a.b@example.com";
+                printf("To: %s\n", recipient);
+                switch (breachType) {
+                    case TOO_LOW:
+                        printf("Hi, the temperature is too low\n");
+                        break;
+                    case TOO_HIGH:
+                        printf("Hi, the temperature is too high\n");
+                        break;
+                    case NORMAL:
+                        // No action needed for normal
+                        break;
+                }
+            }
+            break;
+        default:
+            // Handle unexpected alert target
+            break;
+    }
 }
 
-void sendToController(BreachType breachType) {
-  const unsigned short header = 0xfeed;
-  printf("%x : %x\n", header, breachType);
-}
+// Main function to check temperature and send alerts based on breach and target
+void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+    BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
+    sendAlert(alertTarget, breachType);
+} 
 
-void sendToEmail(BreachType breachType) {
-  const char* recepient = "a.b@c.com";
-  switch(breachType) {
-    case TOO_LOW:
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too low\n");
-      break;
-    case TOO_HIGH:
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too high\n");
-      break;
-    case NORMAL:
-      break;
-  }
-}
