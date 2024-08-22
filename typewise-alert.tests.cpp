8d74574f707ad97e5db911e6f10c64ba49f5e199
VinutaNaik04 <vinutanaik456@gmail.com>
2024-08-22 19:34:25 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -3,7 +3,7 @@
 
 TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
 TEST(InferBreachTest, TemperatureTooLow) {
-  EXPECT_EQ(inferBreach(-1, 0, 35), TOO_LOW);
-  EXPECT_EQ(inferBreach(5, 10, 35), TOO_LOW);
+  EXPECT_EQ(inferBreach(-1, 0, 40), TOO_LOW);
+  EXPECT_EQ(inferBreach(5, 9, 40), TOO_LOW);
 }
 }
